b588e92b7ca757e84e1a067e276c26f756e49d84
FrancescoRisso <francesco@frisso.net>
2019-05-03 21:42:43 +0200

    Fixing a bug about reading current folder

--- functions/config.py
+++ functions/config.py
@@ -1,10 +1,12 @@
 import json
+import os 
 
 
 # Returns a file in which there are all the configs of Settings.json
 def config():
     try:
-        with open('./Settings.json', 'r') as settings_file:
+        file_path = os.path.dirname(os.path.realpath(__file__)) + "/../Settings.json"
+        with open(file_path, 'r') as settings_file:
             return json.load(settings_file)
     except Exception as e:
         print("Error while opening config file: '" + str(e) + "'")
