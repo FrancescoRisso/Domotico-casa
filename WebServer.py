76181d3327deba7435731314e8506e84ce30988a
FrancescoRisso <francesco@frisso.net>
2019-12-08 19:53:23 +0100

    Set connection in the web server to start every time the web server is called, so that the connection doesn't get closed

--- WebServer.py
+++ WebServer.py
@@ -16,6 +16,7 @@ footer = '\n\n\t\t<small>\n\t\t\tLast modified: April 28, 2019\n\t\t</small>\n\t
 # Read a certain temperature from the database
 def read(index):
     reader.execute("SELECT Temperature FROM CURRENT_DATA WHERE InputID = " + str(index))
+
     var = str(reader.fetchone()).replace("(Decimal('", "").replace("'),)","")
     return var
 
@@ -28,6 +29,12 @@ def page_add(page, stanza, temp):
 
 @app.route('/')
 def hello():
+    global reader
+
+    date = get_date()
+    connection = connect(date, "Temperatures")
+    reader = connection.cursor()
+
     page = header
     page = page + "\n\t\t<h1>\n\t\t\tTemperature " + get_date() + "\n\t\t</h1>\n\t\t<hr>"
     page = page + "\n\n\t\t<table>\n\t\t\t<thead>\n\t\t\t\t<tr>\n\t\t\t\t\t<th align = \"left\">Stanza</th>\n\t\t\t\t\t<th align = \"left\">Temperatura</th>\n\t\t\t\t</tr>\n\t\t\t</thead>\n\t\t\t<tbody>"
@@ -38,22 +45,21 @@ def hello():
     page = page_add(page, "Salone", read(3))
     page = page + "\n\t\t\t</tbody>\n\t\t</table>\n\t\t<hr>\n\t\t<p>Per vedere il grafico con gli storici di tutte le temperature <a href =\"https://frisso.grafana.net/d/ri1HUveiz/stanze?orgId=1&refresh=5m&from=now-6h&to=now\">clicca qui</a><hr>"
     page = page + footer
+
+    connection.close
+
     return page
 
 
 if __name__ == '__main__':
 # https://stackoverflow.com/questions/7023052/configure-flask-dev-server-to-be-visible-across-the-network
 # Note: since port 80 is a privileged port, this program has to be started with root permissions.
-    host_url = "192.168.0.2"
-    db = "Temperatures"
-    usr = "prova"
-    pwd = "prova"
 
-    date = get_date()
-    connection = connect(date, "Temperatures")
-    reader = connection.cursor()
+#    date = get_date()
+#    connection = connect(date, "Temperatures")
+#    reader = connection.cursor()
 
-serve(app, listen='*:80')
+    serve(app, listen='*:80')
     #app.run(host= '0.0.0.0', port=80)
 #   Default call to this app
 #   app.run()
