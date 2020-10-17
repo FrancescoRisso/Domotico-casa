c1f62d2e846296a35ece088429c8eb4b317807f8
Fulvio Risso <fulvio.risso@polito.it>
2020-10-17 19:25:58 +0200

    Added the TEMPERATURES_CURRENT update

--- functions/insert.py
+++ functions/insert.py
@@ -6,11 +6,15 @@ def insert(values, writer, date, what):
     try:
         DBnames = "Date"
         DBvalues = f"'{date}'"
+        DBupdate = ""
         for value in values:
             DBnames = f"{DBnames}, {value['name']}"
             DBvalues = f"{DBvalues}, '{value['value']}'"
+            DBupdate = f"{DBupdate}, {value['name']} = {value['value']}"
 
         writer.execute(f"INSERT INTO {what}({DBnames}) VALUES({DBvalues})")
+        if what == "TEMPERATURES":
+            writer.execute(f"UPDATE TEMPERATURES_CURRENT SET {DBupdate[2:]} WHERE Id = 1")
 
     except Error as e:
         print(f"Error while putting data in the database: {e}")
