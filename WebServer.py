b2facccad8bd65861e56ea7de7e70e271420b3e7
FrancescoRisso <49359875+FrancescoRisso@users.noreply.github.com>
2020-12-30 15:17:37 +0100

    Updated to the new database format

--- WebServer.py
+++ WebServer.py
@@ -14,8 +14,8 @@ footer = '\n\n\t\t<small>\n\t\t\tLast modified: April 28, 2019\n\t\t</small>\n\t
 
 
 # Read a certain temperature from the database
-def read(index):
-    reader.execute("SELECT Temperature FROM CURRENT_DATA WHERE InputID = " + str(index))
+def read(room):
+    reader.execute(f"SELECT {room} FROM TEMPERATURES_CURRENT")
 
     var = str(reader.fetchone()).replace("(Decimal('", "").replace("'),)","")
     return var
@@ -38,11 +38,11 @@ def hello():
     page = header
     page = page + "\n\t\t<h1>\n\t\t\tTemperature " + get_date() + "\n\t\t</h1>\n\t\t<hr>"
     page = page + "\n\n\t\t<table>\n\t\t\t<thead>\n\t\t\t\t<tr>\n\t\t\t\t\t<th align = \"left\">Stanza</th>\n\t\t\t\t\t<th align = \"left\">Temperatura</th>\n\t\t\t\t</tr>\n\t\t\t</thead>\n\t\t\t<tbody>"
-    page = page_add(page, "Camera Francesco", read(1))
-    page = page_add(page, "Camera Valentina", read(2))
-    page = page_add(page, "Camera Genitori", read(5))
-    page = page_add(page, "Studio", read(4))
-    page = page_add(page, "Salone", read(3))
+    page = page_add(page, "Camera Francesco", read("Camera_Francesco"))
+    page = page_add(page, "Camera Valentina", read("Camera_Valentina"))
+    page = page_add(page, "Camera Genitori", read("Camera_genitori"))
+    page = page_add(page, "Studio", read("Studio"))
+    page = page_add(page, "Salone", read("Salone))
     page = page + "\n\t\t\t</tbody>\n\t\t</table>\n\t\t<hr>\n\t\t<p>Per vedere il grafico con gli storici di tutte le temperature <a href =\"https://frisso.grafana.net/d/ri1HUveiz/stanze?orgId=1&refresh=5m&from=now-6h&to=now\">clicca qui</a><hr>"
     page = page + footer
 
