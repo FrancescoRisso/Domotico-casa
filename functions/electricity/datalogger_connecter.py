ab1a7b4d59336866af626cea9de1a35c0a991920
Francesco Risso <francesco@frisso.net>
2023-09-27 18:11:06 +0200

    Auto-formatting

--- functions/electricity/datalogger_connecter.py
+++ functions/electricity/datalogger_connecter.py
@@ -7,14 +7,14 @@ from functions.config import config
 
 def dl_connect():
     configuration = config()
-    link = configuration['Datalogger']['ip'] + configuration['Datalogger']['key']
+    link = configuration["Datalogger"]["ip"] + configuration["Datalogger"]["key"]
 
-    try: 
+    try:
         # To fix it, BTicino requires us to connect to the home page ('/')
         # and only after that we can connect to the link with the PUB key
         conn = requests.get(link)
     except Exception:
-        conn = requests.get(configuration['Datalogger']['ip'])
+        conn = requests.get(configuration["Datalogger"]["ip"])
         conn = requests.get(link)
 
     key = str(conn.text)
@@ -22,13 +22,13 @@ def dl_connect():
     key = key[5:29]
 
     i = ConvertiChiave(key)
-    d = configuration['Datalogger']['password']
+    d = configuration["Datalogger"]["password"]
     l = 1
-    o = Calcola(d,i)
+    o = Calcola(d, i)
     W1 = o[0:24]
     W2 = o[24:48]
 
-    link = configuration['Datalogger']['ip'] + configuration['Datalogger']['key_send'] + "?W1=" + W1 + "&W2=" + W2
+    link = configuration["Datalogger"]["ip"] + configuration["Datalogger"]["key_send"] + "?W1=" + W1 + "&W2=" + W2
     conn = requests.get(link)
 
     return conn
@@ -38,13 +38,13 @@ def ConvertiChiave(e):
     t = ""
     n = e[0]
     n = int(n)
-    i = e[n-1]
+    i = e[n - 1]
     i = int(i)
     for d in range(0, 24):
-        if not(24 > i + d):
+        if not (24 > i + d):
             l = 24 - d
             break
-        t += e[i+d]
+        t += e[i + d]
 
     for d in range(0, l):
         t += e[d]
@@ -52,21 +52,20 @@ def ConvertiChiave(e):
 
 
 def Calcola(e, t):
-    l=''
-    i=[]
-    d=[]
-    n=[]
+    l = ""
+    i = []
+    d = []
+    n = []
     for o in range(0, 12):
-        n = n + [int(t[2*o:2*o+2], 16)]
+        n = n + [int(t[2 * o : 2 * o + 2], 16)]
     for o in range(0, len(e)):
         i = i + [ord(e[o])]
     for o in range(len(e), 12):
         i = i + [random.randint(0, 40)]
     for o in range(0, 12):
         d = d + [int(i[o]) * int(n[o])]
-        s = hex(d[o]).lstrip('0x')
+        s = hex(d[o]).lstrip("0x")
         while len(s) < 4:
             s = "0" + s
         l += s
     return l
-
